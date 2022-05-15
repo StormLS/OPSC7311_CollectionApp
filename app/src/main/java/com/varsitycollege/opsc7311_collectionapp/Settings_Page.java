989437f9c2283f66818186c86da586db60b563f8
amber <amber.bruil@gmail.com>
2022-05-15 21:44:50 +0200

    settings page

---
+++ app/src/main/java/com/varsitycollege/opsc7311_collectionapp/Settings_Page.java
@@ -0,0 +1,14 @@
+package com.varsitycollege.opsc7311_collectionapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Settings_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
